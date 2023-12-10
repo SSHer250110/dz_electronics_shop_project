541c6397123b4f5d097e6369d6c4c8f2a8ce6a1e
Sergey_Shershikov <ssher250110@yandex.ru>
2023-12-10 20:32:46 +0300

    added tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_calculate_total_price(test_product):
+    assert test_product.calculate_total_price() == 2000.0
+
+
+def test_apply_discount(test_product):
+    assert test_product.price == 100.00
+    test_product.pay_rate = 0.5
+    test_product.apply_discount()
+    assert test_product.price == 50.0
