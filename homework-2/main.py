8a3b71b1ecf2df55a1c378ecd9a40e5540df6676
Sergey_Shershikov <ssher250110@yandex.ru>
2023-12-18 17:48:35 +0300

    added path Item.instantiate_from_csv

--- homework-2/main.py
+++ homework-2/main.py
@@ -1,4 +1,5 @@
 from src.item import Item
+from utils import FILE_CSV
 
 if __name__ == '__main__':
     item = Item('Телефон', 10000, 5)
@@ -10,7 +11,7 @@ if __name__ == '__main__':
     # длина наименования товара больше 10 символов
     item.name = 'СуперСмартфон'
     # Exception: Длина наименования товара превышает 10 символов.
-    Item.instantiate_from_csv('../src/items.csv')  # создание объектов из данных файла
+    Item.instantiate_from_csv(FILE_CSV)  # создание объектов из данных файла
     assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
 
     item1 = Item.all[0]
