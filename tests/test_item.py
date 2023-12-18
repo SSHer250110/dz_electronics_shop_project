c7ff75d815a0aebedbc4e0bed95e7eebd257bae7
Sergey_Shershikov <ssher250110@yandex.ru>
2023-12-18 17:50:48 +0300

    tests hw-2

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,5 +1,6 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
+from utils import FILE_TEST_CSV
 
 
 def test_calculate_total_price(test_product):
@@ -11,3 +12,22 @@ def test_apply_discount(test_product):
     test_product.pay_rate = 0.5
     test_product.apply_discount()
     assert test_product.price == 50.0
+
+
+def test_instantiate_from_csv():
+    """
+    Тесты класс метода, через тестовый файл csv в пакете tests
+    """
+    Item.instantiate_from_csv(FILE_TEST_CSV)
+    assert len(Item.all) == 6
+    item1 = Item.all[5]
+    assert int(item1.quantity) == 10
+
+
+def test_string_to_number():
+    """
+    Тесты статического метода
+    """
+    assert Item.string_to_number("10.25") == 10
+    assert Item.string_to_number("0") == 0
+    assert Item.string_to_number("-7") == -7
