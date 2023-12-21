d96226e1fd288a7ad5858f22938cf1365cf4643d
Sergey_Shershikov <ssher250110@yandex.ru>
2023-12-21 21:40:31 +0300

    added docstring

--- src/phone.py
+++ src/phone.py
@@ -8,9 +8,15 @@ class Phone(Item):
         self._number_of_sim = number_of_sim
 
     def __repr__(self):
+        """
+        Метод для отображения информации в режиме отладки.
+        """
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
     def __str__(self):
+        """
+        Метод для отображения информации для пользователя.
+        """
         return self.name
 
     @property
@@ -19,6 +25,9 @@ class Phone(Item):
 
     @number_of_sim.setter
     def number_of_sim(self, number_of_sim):
+        """
+        Метод с условиями, присваивающий новое значение атрибуту
+        """
         if isinstance(number_of_sim, int) and number_of_sim > 0:
             self._number_of_sim = number_of_sim
         else:
