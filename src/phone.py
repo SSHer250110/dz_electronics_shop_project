25fef8a12b2ebe7b06abdcaa26ba4e46baa8d583
Sergey_Shershikov <ssher250110@yandex.ru>
2023-12-21 21:48:56 +0300

    added docstrings

--- src/phone.py
+++ src/phone.py
@@ -2,8 +2,19 @@ from src.item import Item
 
 
 class Phone(Item):
+    """
+    Класс описывающий товар телефон
+    """
 
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
+        """
+        Создание экземпляра класса phone.
+
+        :param name: Название товара.
+        :param price: Цена за единицу товара.
+        :param quantity: Количество товара в магазине.
+        :param quantity: Количество поддерживаемых сим-карт.
+        """
         super().__init__(name, price, quantity)
         self._number_of_sim = number_of_sim
 
