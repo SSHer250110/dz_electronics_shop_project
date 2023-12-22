7f1106feae7669a2671ab050aca9fd429cd7e269
Sergey_Shershikov <ssher250110@yandex.ru>
2023-12-23 02:54:46 +0300

    added class mixinkeyborad

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,6 +1,22 @@
 from src.item import Item
 
 
+class MixinKeyboard:
+
+    def __init__(self):
+        self._language = "EN"
+
+    def change_lang(self):
+        if self._language == "EN" or self._language == "RU":
+            if self._language == "EN":
+                self._language = "RU"
+            else:
+                self._language = "EN"
+            return self._language
+        else:
+            raise AttributeError("property 'language' of 'Keyboard' object has no setter")
+
+
 class Keyboard(Item, MixinKeyboard):
     def __init__(self, name: str, price: float, quantity: int):
         super().__init__(name, price, quantity)
